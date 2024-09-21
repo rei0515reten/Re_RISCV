be4979f5ca2ce87d6c3bae9d0cf1bb349f0fe167
rei0515reten <glay5152@gmail.com>
2024-09-21 23:49:42 +0900

    Add adder module

--- core/adder/adder.sv
+++ core/adder/adder.sv
@@ -1,4 +1,8 @@
-module adder(input [31:0]a,b,
-            output [31:0]out);
-assign out = a + b;
+module adder(input[31:0] a,
+	input[31:0] b,
+	input[31:0] x,
+	output[31:0] out);
+
+	//x→繰り上がり
+	assign out = a + b + x;
 endmodule
