c695c620a782abbbe8299472b3beadc3eb71dcf8
rei0515reten <glay5152@gmail.com>
2024-10-14 20:49:58 +0900

    Move dir

--- core/adder/adder.sv
+++
@@ -1,8 +0,0 @@
-module adder(input[31:0] a,
-	input[31:0] b,
-	input[31:0] x,
-	output[31:0] out);
-
-	//x→繰り上がり
-	assign out = a + b + x;
-endmodule
--- core/mux2/mux2.sv
+++
@@ -1,6 +0,0 @@
-module mux2(input logic [31:0] d0, d1,
-            input logic s,
-            output logic [31:0] out);
-
-    assign out = s ? d1 : d0;
-endmodule
