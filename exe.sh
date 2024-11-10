8a996c5788a57ffdc1ca08d252756423edb1e315
rei0515reten <glay5152@gmail.com>
2024-11-10 22:19:51 +0900

    Add extend Change exe.sh

--- exe.sh
+++ exe.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 iverilog -g 2012 core/*.sv core/circuit/*.sv core/controller/*.sv core/datapath/*.sv memory/*.sv top.sv testbench/sim_test.sv
 
-vvp a.out
+#vvp a.out
 
-gtkwave sim.vcd
+#gtkwave sim.vcd
