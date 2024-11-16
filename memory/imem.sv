a087af613c4e2c056890d34f54467e006c701c94
rei0515reten <glay5152@gmail.com>
2024-11-17 02:02:54 +0900

    Change imem

--- memory/imem.sv
+++ memory/imem.sv
@@ -4,8 +4,8 @@ module imem(input logic [31:0]a,
     logic [31:0] RAM[63:0];
 
     initial
-        $readmemh("riscvtest.txt",RAM);
+        $readmemh("memory/riscvtest.txt",RAM);
 
-    //assignで常に繋げる
+    //1wordずつ取り出す
     assign rd = RAM[a[31:2]];
 endmodule
