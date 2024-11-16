00e1165a53a53191c4e53d60b94e6e3094e7ece8
rei0515reten <glay5152@gmail.com>
2024-11-17 02:29:45 +0900

    Change dir

--- core/circuit/mux2.sv
+++
@@ -1,7 +0,0 @@
-module mux2 #(parameter WIDTH = 8)
-		(input logic [WIDTH-1:0] d0, d1,
-            	input logic s,
-            	output logic [WIDTH-1:0] out);
-
-    assign out = s ? d1 : d0;
-endmodule
--- exe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-iverilog -g 2012 core/*.sv core/circuit/*.sv core/controller/*.sv core/datapath/*.sv memory/*.sv top.sv testbench/sim_test.sv
-
-vvp a.out
-
-gtkwave sim.vcd
